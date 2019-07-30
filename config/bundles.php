11e75c59cd4152ff32c30619b2bb78d698804091
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-07-30 11:48:47 +0200

    Chap 11: Thumbnailing with LiipImagineBundle

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
